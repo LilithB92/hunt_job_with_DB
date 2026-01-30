3fbe48c5038b1c1646d0613f22ec31bb80d40501
Lilith <bichakhchyanlilith1992@gmail.com>
2026-01-30 22:01:21 +0300

    init test_postgres_db with its tests

--- tests/test_postgres_db.py
+++ tests/test_postgres_db.py
@@ -1,12 +1,13 @@
-import psycopg2
-import os
-from dotenv import load_dotenv
-from psycopg2 import Error
-
 from src.postgres_db import PostgresDB
 
-load_dotenv()
 
-def test_postgres_data_init()->None:
-    postgres_db =PostgresDB()
+def test_postgres_data_init() -> None:
+    postgres_db = PostgresDB()
+    assert postgres_db.conn is None
+
+
+def test_close() -> None:
+    postgres_db = PostgresDB()
+    postgres_db.connect()
+    postgres_db.close()
     assert postgres_db.conn is None
